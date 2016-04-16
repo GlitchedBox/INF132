632af476e01c68572bcebe682800193cf75fe4d8
GlitchedBox <i.flip.book.pages@gmail.com>
2016-04-15 21:34:03 -0700

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,147 +1,192 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title>iXercise</title>
+// THESE ARE SIMPLE FUNCTIONS THAT ARE USED THROUGHOUT ALL OF THE PAGES
 
-    <link rel="stylesheet" type="text/css" href="jquery/jquery.autocomplete.css" />   
-    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+// These are the variables that will be used throughout the entire program and their
+// values will be from the databases
 
-    <link rel="stylesheet" href="style.css">
-    <script type="text/javascript" src="functions.js"></script>
-    
-    <script>
-    
-        $(document).ready(function(){
+var current_user_id = 0;
+var current_user_postion = "";
 
-            $.each( userDatabase, function(index, p){
-                var html = "<tr><td>"+ "<button type='button' class='modal_button' data-toggle='modal' data-target='#userModal'>" + p.id + "</button>" + "</td><td>" + p.role + "</td></tr>";
-
-                //var html = "<tr><td>"+ "<button type='button' class='btn btn-info btn-sm' data-toggle='modal' data-target='#myModal'>" + p.id + "</button>" + "</td><td>" + p.role + "</td></tr>";
-
-                $("#user_table").append(html);
-
-            });
-
-            $( "#add_user_button" ).click(function() { 
-                //alert($('#new_user_id').val());
-                //alert($('#new_user_position').val());
-                // UPDATE THE DATABASE INFORMATION OF THE USER
-                document.location.href='userList.html';
-            });
-
-            $( "#remove_user_button" ).click(function() { 
-                //alert($('#remove_user_id').val());
-                // UPDATE THE DATABASE INFORMATION OF THE USER
-                document.location.href='userList.html';
-            });
-
-            // Puts the number of the user chosen on the modal title 
-            $(".modal_button").click(function() {
-                $(".modal-title").html($(this).text() + " Profile");
-
-                // WILL UPDATE THE CURRENT USER CHOSEN ADMIN VAR HERE BEFORE THE REST BELOW
-
-                $(".user_name").html(current_user_chosen_admin.first_name + " " + current_user_chosen_admin.last_name);
-                $(".user_email").html(current_user_chosen_admin.email);
-                $(".user_contact").html(current_user_chosen_admin.contact);
-            });
-
-        });
-
-    </script>
-
-</head>
-<body id="admin-body">
-    <div class="container">
-        <h1 id="admin-title"> iXercise Administrator System </h1>
-        <hr>
-
-        <a href="loginScreen.html" style="float: right"> signOut </a>
+/// WEB APP USER DATABASE
+var current_user_info = { 
+					      id: "id",
+					      uid: "UCI_STUDENT_ID",
+					      first_name: "Sarah",
+					      middle_name: "K.",
+					      last_name: "Johnson",
+					      dob: "date_of_birth",
+					      gender: "female",
+					      office: "XXX building Room 215",
+					      email: "asdf@abc.org",
+					      contact: "949-123-1234",
+					      position: "doctor",
+					      title: "Doctor",
+					      year_entered: "2015",
+					      reports_to: [ {id: "0198475"},  {id: "1726548"} ],
+					      patients: [ {id: "1234567"}, {id: "89101112"} ],
+					      picture: "filename_id.jpg",
+					      role: ["doctor"]
+					    };
 
 
-        <!-- This will be the title and the column labels -->
-        <div class="row">
-            <div class="col-md-12">
-                <h4> Add/Remove Users </h4>
-                <table class="table table-bordred table-striped" border='0' width='50%' align='center' style="border-collapse:collapse" cellspacing='3' cellpadding='5'>
-                    <tr>
+var current_user_chosen_admin = { 
+					      id: "id",
+					      uid: "UCI_STUDENT_ID",
+					      first_name: "first",
+					      middle_name: "middle",
+					      last_name: "last",
+					      dob: "date_of_birth",
+					      gender: "female",
+					      office: "XXX building Room 215",
+					      email: "asdf@abc.org",
+					      contact: "949-123-1234",
+					      position: "doctor",
+					      title: "Doctor",
+					      year_entered: "2015",
+					      reports_to: [ {id: "0198475"},  {id: "1726548"} ],
+					      patients: [ {id: "1234567"}, {id: "89101112"} ],
+					      picture: "filename_id.jpg",
+					      role: [ "admin"]
+					    };
+////////////////////
 
-                            <td> user id </td>
-                            <td><input name="new_user_id" type="text" id="new_user_id" placeholder="#######" size="20"/></td>
-                            
-                            <td> Occupation </td>
-                            <td><input name="new_user_position" type="text" id="new_user_position" placeholder="doctor, trainer, admin" size="20"/></td>
-                            
-                            <td><input name="add_user_button" type="button" id="add_user_button" size="20" value="Add" /></td>
-                    </tr>
+///// TA GAME DATABASE
+var current_user_patient_chosen = {
+									id: "1234567",
+									first_name: "patient_first",
+									last_name: "patient_last",
+									dob: "date_of_birth",
+									gender: "male",
+									email: "blah@uci.edu",
+									wattage: "100 Watts",
+									heart_rate: "150 beats per minute",
+									workload: "1hr",
+									game: "Minecraft",
+									prescription: "something here, something here, something here, and more stuff"
+								  };
 
-                    <tr>
-                            <td> user id </td>
-                            <td><input name="remove_user_id" type="text" id="remove_user_id" placeholder="#######" size="20"/></td>
-                            
-                            <td></td><td></td>
-                            <td><input name="remove_user_button" type="button" id="remove_user_button" size="20" value="Remove" /></td>
-                    </tr>
-                </table>
+/////////////////////
 
+//// ADMIN DATABASE
+var userDatabase = [{id:"1234567", pass:"password1", role:["admin", "doctor"]}, {id:"0987654", pass:"password2",role:["trainer"]}, {id:"7654321", pass:"password3",role:["admin"]}, {id:"1357986", pass:"password4",role:["patient"]}];
+///////////////////
 
-                <div id="user_list_div"> 
-                <hr>
-                <h4> User List </h4>
-
-                <table id='user_table' class="table table-bordred table-striped" border='0' width='50%' align='center' style="border-collapse:collapse" cellspacing='3' cellpadding='5'></table>
-                </div>
-            </div> 
-
-
-          <!-- Modal -->
-          <div class="modal fade" id="userModal" role="dialog">
-            <div class="modal-dialog">
-            
-              <!-- Modal content-->
-              <div class="modal-content">
-                <div class="modal-header">
-                  <button type="button" class="close" data-dismiss="modal">&times;</button>
-                  <h4 class="modal-title"> </h4>
-                </div>
-                <div class="modal-body">
-                            <div class='panel panel-info'>
-                                <div class='panel-body'>
-                                  <div class='row'>
-                                    <div class='col-md-3 col-lg-3 ' align='center'> <img alt='User Pic' src='#' class='img-circle img-responsive'> 
-                                    </div>
-                                <div class='col-md-9 col-lg-9'> 
-                                  <table class='table table-user-information'>
-                                    <tbody>
-                                     <tr>
-                                        <td>Name:</td>
-                                        <td class="user_name"> </td>
-                                      </tr>
-                                      <tr>
-                                        <td>Email:</td>
-                                        <td class="user_email"> </td>
-                                      </tr>
-                                    </tbody>
-                                  </table>
-                                </div>
-                              </div>
-                            </div>
-
-                </div>
-                <div class="modal-footer">
-                  <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                </div>
-              </div>
-              
-            </div>
-          </div> 
+// This function is for when the user logs in, it will take them to the correct page
+$(function(){
 
 
-    </div> <!-- container end -->
+	  $('#login-form').submit(function() {
+
+	  	$.each( userDatabase, function(index, p){
 
 
-</body>
-</html>
+		    if ($("#lg_id").val() == p.id && $("#lg_password").val() == p.pass && $.inArray('doctor', p.role) != -1){ 
+		    	current_user_id = p.id;
+		    	//alert($("#lg_id").val());
+		    	//alert($("#lg_password").val());
+		    	$("#login-form").attr('action', 'patientListD.html'); 
+		    } 
+
+		    else if ($("#lg_id").val() == p.id && $("#lg_password").val() == p.pass && $.inArray('trainer', p.role) != -1){ 
+		    	current_user_id = p.id;
+		    	//alert($("#lg_id").val());
+		    	//alert($("#lg_password").val());
+		    	$("#login-form").attr('action', 'patientListT.html'); 
+		    } 
+		    else if ($("#lg_id").val() == p.id && $("#lg_password").val() == p.pass && $.inArray('admin', p.role) != -1){ 
+		    	current_user_id = p.id;
+		    	//alert($("#lg_id").val());
+		    	//alert($("#lg_password").val());
+		    	$("#login-form").attr('action', 'userList.html'); 
+		    } 
+		    else if ($("#lg_id").val() == p.id && $("#lg_password").val() == p.pass && $.inArray('patient', p.role) != -1){ 
+		    	current_user_id = p.id;
+		    	//alert($("#lg_id").val());
+		    	//alert($("#lg_password").val());
+		    	$("#login-form").attr('action', 'patientPage.html'); 
+		    } 
+		    else if ($("#lg_id").val() == p.id && $("#lg_password").val() != p.pass){ 
+		    	alert("incorrect id or password");
+		    } 
+
+	// THIS WAS ADDED FOR TESTING PURPOSES - DEFAULT TO THE DOCTORS PAGE
+		   else {
+		    	$("#login-form").attr('action', 'patientListD.html'); 
+		   }
+
+
+	  	});
+
+	  });
+
+});
+
+
+// This is all of the functions in the patientList page
+$(function(){
+
+	// This adds the patients already in the database
+    $.each( current_user_info.patients, function(index, p){
+        var html = "<tr><td>"+ "<button type='button' class='modal_button' data-toggle='modal' data-target='#patientListModal'>" + p.id + "</button>" + "</td></tr>";
+
+        $("#patient_table").append(html);
+    });
+
+	// DOCTOR
+    // code when user presses the add patient button
+    $( "#add_patient_buttonD" ).click(function() { 
+        //alert($('#new_patient_id').val());
+        // UPDATE THE ADMIN DATABASE INFORMATION OF THE USER
+        document.location.href='patientListD.html';
+	});
+
+    // code when user presses the remove button
+    $( "#remove_patient_buttonD" ).click(function() { 
+        //alert($('#remove_patient_id').val());
+        // UPDATE THE ADMIN DATABASE INFORMATION OF THE USER
+        document.location.href='patientListD.html';
+	});
+
+	// TRAINER
+    // code when user presses the add patient button
+    $( "#add_patient_buttonT" ).click(function() { 
+        //alert($('#new_patient_id').val());
+        // UPDATE THE ADMIN DATABASE INFORMATION OF THE USER
+        document.location.href='patientListT.html';
+	});
+
+    // code when user presses the remove button
+    $( "#remove_patient_buttonT" ).click(function() { 
+        //alert($('#remove_patient_id').val());
+        // UPDATE THE ADMIN DATABASE INFORMATION OF THE USER
+        document.location.href='patientListT.html';
+	});
+
+});
+
+
+// Patient page edit prescription
+$(function(){
+
+	// clicked on the edit button on the prescription and treatement section
+	$("#edit_patient_treatement").click(function() {
+		document.location.href='editPatientProfile.html';
+	});
+
+
+	$('#edit-patient-profile-form').submit(function() {
+
+		var prescri = $('#edit-patient-profile-form').find('input[name="presc"]').val();
+		var treate = $('#edit-patient-profile-form').find('input[name="treat"]').val();
+
+		if (prescri != ""){
+			// HERE WE WILL REPLACE THE USER DATABASE WITH THIS INFORMATION
+			//alert("prescription");
+		}
+		if (treate != ""){
+			// HERE WE WILL REPLACE THE USER DATABASE WITH THIS INFORMATION
+			//alert("treatment");
+		}
+
+	});
+
+});
